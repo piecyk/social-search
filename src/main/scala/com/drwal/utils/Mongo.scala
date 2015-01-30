b7dc3118e778bf28a5408f576023b291e9cd644a
Damian Pieczynski <Damian.Pieczynski@capita.co.uk>
2015-01-30 13:42:14 +0000

    dev: fix for heroku

--- src/main/scala/com/drwal/utils/Mongo.scala
+++ src/main/scala/com/drwal/utils/Mongo.scala
@@ -7,20 +7,20 @@ import reactivemongo.api.MongoDriver
 
 trait Mongo {
 
-  import scala.concurrent.ExecutionContext.Implicits.global
+  //import scala.concurrent.ExecutionContext.Implicits.global
 
   println("Mongo")
 
-  val driver = new MongoDriver
-  val connection = driver.connection(List(MongoConfig.url))
-  val db = connection(MongoConfig.database)
+  //val driver = new MongoDriver
+  //val connection = driver.connection(List(MongoConfig.url))
+  //val db = connection(MongoConfig.database)
 
-  object MongoUserCollection {
-    val userCollection = db("user.collection")
+  //object MongoUserCollection {
+  //val userCollection = db("user.collection")
 
-    lazy val userDao: UserDao = new UserReactiveDao(db, userCollection, system)
-    userDao.remove()
-    println("MongoUserCollection")
-  }
+  //lazy val userDao: UserDao = new UserReactiveDao(db, userCollection, system)
+  //userDao.remove()
+  //println("MongoUserCollection")
+  //}
 
 }
